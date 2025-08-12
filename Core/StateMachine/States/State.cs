a54536fae6a007a1ab128fb3e5fdddb6d3b13f80
dreml <nik.dreml@gmail.com>
2025-08-12 23:26:23 +0400

    added base skeleton actor

--- Core/StateMachine/States/State.cs
+++ Core/StateMachine/States/State.cs
@@ -7,7 +7,7 @@ public abstract partial class State : Node
   protected AnimationPlayer AnimPlayer { get; private set; }
   protected Sprite2D Sprite { get; private set; }
   protected PhysicsComponent Physics { get; private set; }
-  protected PlayerController Controller { get; private set; }
+  protected BaseController Controller { get; private set; }
 
   protected virtual bool CanAttack => true;
 
@@ -29,6 +29,6 @@ public abstract partial class State : Node
     AnimPlayer = OwnerCharacter.GetNode<AnimationPlayer>("AnimationPlayer");
     Sprite = OwnerCharacter.GetNode<Sprite2D>("Sprite");
     Physics = OwnerCharacter.GetNode<PhysicsComponent>("PhysicsComponent");
-    Controller = OwnerCharacter.GetNode<PlayerController>("PlayerController");
+    Controller = OwnerCharacter.GetNode<BaseController>("Controller");
   }
 }
